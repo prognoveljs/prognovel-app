7197698e9a1a133d0cecbf36edf5346ae60b7334
ProgNovel <prognovel@gmail.com>
2022-02-27 18:56:38 +0800

    try redirect response in sw

--- src/service-worker/redirect.ts
+++ src/service-worker/redirect.ts
@@ -1,16 +1,19 @@
 export const isReadPage = "/read/";
 
 export async function redirect(cache: Cache, path: string) {
-  const response = await cache.match("/read/");
-  if (!response) return;
-  const html = await response.text();
-  const init = {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  };
-  return new Response(
-    html.replace("</head>", `<meta name="redirect" content="${path}" /></head>`),
-    init,
-  );
+  return Response.redirect("/read?load=" + path.split("/read/")[1]);
 }
+// export async function redirect(cache: Cache, path: string) {
+//   const response = await cache.match("/read/");
+//   if (!response) return;
+//   const html = await response.text();
+//   const init = {
+//     headers: {
+//       "Content-Type": "text/html",
+//     },
+//   };
+//   return new Response(
+//     html.replace("</head>", `<meta name="redirect" content="${path}" /></head>`),
+//     init,
+//   );
+// }
