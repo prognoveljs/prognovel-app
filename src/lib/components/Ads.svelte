029b1c4088eb715402fac5d0f5a7ef7bd2862546
ProgNovel <prognovel@gmail.com>
2022-04-09 03:34:18 +0800

    ads component

--- src/lib/components/Ads.svelte
+++ src/lib/components/Ads.svelte
@@ -1,17 +1,46 @@
 <script>
+  import { isWebMonetizationActive } from "$lib/utils/web-monetization";
+
   export let width = "100%";
   export let height = "190px";
   export let grid = "";
+
+  let adsenseClient = "";
+  export let adSlot = "";
 </script>
 
 <div class="ads" style="width: {width}; height: {height}; grid-area: {grid}">
-  <span>Ads</span>
+  {#if adsenseClient && $isWebMonetizationActive !== undefined}
+    <div class="ads-widget-container">
+      <ins
+        class="adsbygoogle"
+        style="display:block"
+        data-ad-client={adsenseClient}
+        data-ad-slot={adSlot}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
+  {:else}
+    <span>Ads</span>
+  {/if}
 </div>
 
-<style>
+<style lang="scss">
   .ads {
     display: flex;
     background: #666a;
+    margin: var(--margin, auto);
+    position: relative;
+
+    &::after {
+      content: "promoted";
+      position: absolute;
+      top: -1.6em;
+      right: 0;
+      // font-style: italic;
+      opacity: 0.7;
+    }
   }
 
   span {
