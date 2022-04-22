9c8e1c98accc3ea11d991963c0a290b822c496d5
ProgNovel <prognovel@gmail.com>
2022-04-23 01:31:49 +0800

    ui: improve keyboard navigation on search result

--- src/lib/components/IconSVG.svelte
+++ src/lib/components/IconSVG.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
   export let data = "";
-  export let size = "24";
+  export let size = "24px";
+  export let style = "";
   $: svg = data.replace('width="24" height="24"', `width="${size}" height="${size}"`);
+  // $: svg = data.replace('width="24" height="24"', `width="100%" height="100%"`);
 </script>
 
-<span on:click>
+<span on:click {style}>
   {@html svg}
 </span>
 
 <style lang="scss">
   span {
     color: var(--color, currentColor);
-    // :global(svg) {
-
-    // }
+    display: inline-flex;
+    align-items: center;
+    :global(svg) {
+      display: block;
+    }
   }
 </style>
--- src/lib/components/navigation/NavSearch.svelte
+++ src/lib/components/navigation/NavSearch.svelte
@@ -1,35 +1,88 @@
 <script lang="ts">
   import { siteMetadata } from "$lib/store/states";
   import { tick } from "svelte";
-  import { fly } from "svelte/transition";
+  import { fly, fade } from "svelte/transition";
   import { getCoverURLPath } from "$lib/utils/images";
   import { stringSearch } from "$lib/utils/string";
   import IconSvg from "$lib/components/IconSVG.svelte";
   import searchIcon from "$lib/assets/feather-icons/search.svg?raw";
+  import arrowDownIcon from "$lib/assets/feather-icons/arrow-down.svg?raw";
+  import alertIcon from "$lib/assets/feather-icons/alert-octagon.svg?raw";
+  import { browser } from "$app/env";
+  import { cubicIn, cubicOut } from "svelte/easing";
+  import { out } from "$lib/utils/animation/animation-keyframes";
 
   export let isMobile: boolean = false;
+  const SEARCH_ROW_ITEMS = 3;
+  const RESULT_WIDTH = 510;
   let search: string;
   let input: HTMLInputElement;
+  let selectedNovel: number = -1;
+  $: if (search === null || selectedNovel < -1) selectedNovel = -1;
+  $: if (selectedNovel > -1 && browser) {
+    const sel = document.querySelectorAll(".result a")?.[selectedNovel] as HTMLAnchorElement;
+    if (sel) sel.focus();
+  }
   $: novelsGroup = $siteMetadata?.novelsMetadata || [];
   $: searchedNovels = !search
     ? []
+    : import.meta.env.IS_DEMO
+    ? Array(11).fill({ id: "yashura-legacy", title: "Yashura Legacy" })
     : novelsGroup.filter((novelMetadata) => {
         return stringSearch((novelMetadata?.title || "").toLowerCase(), search.toLowerCase()) > 0;
       });
 
   function pressKey(e) {
+    console.log(e.key);
     switch (e.key) {
       case "/":
         input.focus();
+        e.preventDefault();
         break;
       case "Escape":
+        search = "";
         input.blur();
+        e.preventDefault();
+        break;
+      case "ArrowLeft":
+        if (!searchedNovels.length || document.activeElement === input) break;
+        selectedNovel--;
+        if (selectedNovel < 0) input.focus();
+        e.preventDefault();
+        break;
+      case "ArrowRight":
+        if (!searchedNovels.length || document.activeElement === input) break;
+        if (selectedNovel < searchedNovels.length) selectedNovel++;
+        e.preventDefault();
+        break;
+      case "ArrowUp":
+        if (!searchedNovels.length) break;
+        selectedNovel -= SEARCH_ROW_ITEMS;
+        if (selectedNovel < 0) input.focus();
+        console.log(selectedNovel);
+        e.preventDefault();
+
+        break;
+      case "ArrowDown":
+        if (!searchedNovels.length) break;
+        if (document.activeElement === input) {
+          selectedNovel = 0;
+        } else {
+          if (selectedNovel + SEARCH_ROW_ITEMS < searchedNovels.length) {
+            selectedNovel += SEARCH_ROW_ITEMS;
+          } else {
+            selectedNovel = searchedNovels.length - 1;
+          }
+        }
+        e.preventDefault();
         break;
     }
   }
 
   function onBlur() {
     setTimeout(() => {
+      const activeEl = document.activeElement;
+      if (activeEl.classList.contains("search-result-item") || activeEl === input) return;
       search = "";
     }, 100);
   }
@@ -39,7 +92,7 @@
   }
 </script>
 
-<svelte:window on:keyup={pressKey} />
+<svelte:window on:keydown={pressKey} />
 
 <section class="input" class:isMobile>
   <div class="side-overlay" />
@@ -53,37 +106,70 @@
   <span class="icon">
     <IconSvg --color="#0006" data={searchIcon} />
   </span>
-  <section class="result">
-    {#each searchedNovels as novel, index}
-      <a
-        sveltekit:prefetch
-        in:fly={{ duration: 125, y: 12, delay: 50 * index }}
-        out:fly={{ duration: 80, y: -20, delay: 30 * index }}
-        href="/novel/{novel.id}"
-        on:click={onClickItem}
-      >
-        <picture>
-          <source
-            srcset={getCoverURLPath(novel.id, { width: 128, height: 128 }, "webp")}
-            type="image/webp"
-          />
-          <source
-            srcset={getCoverURLPath(novel.id, { width: 128, height: 128 }, "jpeg")}
-            type="image/jpeg"
-          />
-          <img
-            src={getCoverURLPath(novel.id, { width: 128, height: 128 }, "jpeg")}
-            alt={novel.title}
-          />
-        </picture>
-        {novel.title}
-      </a>
-    {/each}
-    {#if search && !searchedNovels.length}
-      <div class="no-match">no match found...</div>
-    {/if}
-    <div class:active={search} class="overlay" />
-  </section>
+  {#if search}
+    <section class="result" style="--result-width: {RESULT_WIDTH}px;">
+      <div in:fade={{ duration: 100 }} out:fade={{ duration: 100 }}>
+        <em
+          ><IconSvg
+            --color="#fff7"
+            style="margin-right: 6px;"
+            size="1.1em"
+            data={arrowDownIcon}
+          />use arrow keys to navigate</em
+        >
+        {#if import.meta.env.IS_DEMO}
+          <em
+            ><IconSvg
+              --color="#fff7"
+              style="margin-right: 6px;"
+              size="1.1em"
+              data={alertIcon}
+            />lots novels being added for demo purpose</em
+          >
+        {/if}
+      </div>
+      <div class="item-wrapper">
+        {#each searchedNovels as novel, index}
+          <a
+            sveltekit:prefetch
+            in:fly={{ duration: 125, y: 12, delay: 50 * index }}
+            out:fly={{ duration: 80, y: -20, delay: 30 * index }}
+            id="search-{novel}"
+            class="search-result-item"
+            href="/novel/{novel.id}"
+            on:click={onClickItem}
+            on:blur={onBlur}
+            on:mouseover={() => (selectedNovel = index)}
+          >
+            <picture>
+              <source
+                srcset={getCoverURLPath(novel.id, { width: 128, height: 128 }, "webp")}
+                type="image/webp"
+              />
+              <source
+                srcset={getCoverURLPath(novel.id, { width: 128, height: 128 }, "jpeg")}
+                type="image/jpeg"
+              />
+              <img
+                src={getCoverURLPath(novel.id, { width: 128, height: 128 }, "jpeg")}
+                alt={novel.title}
+              />
+            </picture>
+            {novel.title}
+          </a>
+        {/each}
+      </div>
+      {#if search && !searchedNovels.length}
+        <div class="no-match">no match found...</div>
+      {/if}
+      <div
+        class:active={search}
+        in:fly={{ duration: 225, x: RESULT_WIDTH, easing: cubicOut }}
+        out:fly={{ duration: 300, x: RESULT_WIDTH, easing: cubicIn }}
+        class="overlay"
+      />
+    </section>
+  {/if}
 </section>
 
 <style lang="scss">
@@ -167,49 +253,71 @@
     }
   }
 
-  $resultWidth: 400px;
+  $resultWidth: var(--result-width);
+  $overlay-index: 400;
   .result {
     position: fixed;
     top: var(--header-height);
     right: 0;
-    margin: 1em;
-    display: flex;
+    padding: 1em;
     width: $resultWidth;
-    align-items: start;
-    justify-content: end;
-    flex-wrap: wrap;
-    flex-direction: row-reverse;
-    gap: 1em;
     max-width: 100%;
 
-    a {
+    em {
       display: flex;
-      flex-direction: column;
-      flex: 0;
-      text-align: center;
-      justify-content: center;
-      align-items: center;
-      border-radius: 2px;
-      padding: 0.25em;
-      line-height: 1.1;
-      z-index: 900;
       position: relative;
-      color: var(--primary-color-darken-4);
-      font-weight: 700;
+      z-index: $overlay-index + 1;
+      margin: 0.3em 0 0.3em 0;
+      padding: 0 4px;
+      color: var(--primary-color-lighten-4);
+      background-color: #0006;
+      width: fit-content;
 
-      &:hover {
-        background-color: #fffa;
-        box-shadow: 0 2px 8px #0002;
-      }
-
-      picture {
-        border-radius: 2px;
-        margin-bottom: 8px;
-        height: 128px;
+      &:nth-child(2) {
+        margin-bottom: 0.7em;
       }
     }
 
-    $overlay-index: 400;
+    .item-wrapper {
+      display: flex;
+      align-items: start;
+      justify-content: start;
+      flex-wrap: wrap;
+      // flex-direction: row-reverse;
+      gap: 1em;
+
+      a {
+        display: flex;
+        flex-direction: column;
+        flex: 0;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        border-radius: 2px;
+        padding: 0.25em;
+        line-height: 1.1;
+        z-index: 900;
+        position: relative;
+        color: var(--primary-color-darken-4);
+        font-weight: 700;
+
+        // &:hover:not(:focus),
+        &:focus,
+        &:active {
+          background-color: #fffa;
+          box-shadow: 0 2px 8px #0002;
+          color: var(--primary-color-darken-2);
+          outline: none;
+        }
+
+        picture {
+          border-radius: 2px;
+          margin-bottom: 8px;
+          height: 128px;
+        }
+      }
+    }
+
     .no-match {
       position: absolute;
       top: 0;
@@ -228,11 +336,12 @@
       z-index: $overlay-index;
       background: #fff4;
       backdrop-filter: blur(10px);
+      box-shadow: -4px 0 8px #0001, -4px 0 20px #0001;
       // background: linear-gradient(to left, #0007 30%, #0002, #0000);
-      transform: translateX(#{$resultWidth});
-      &.active {
-        transform: translateX(0);
-      }
+      // transform: translateX(#{$resultWidth});
+      // &.active {
+      //   transform: translateX(0);
+      // }
     }
   }
 </style>
